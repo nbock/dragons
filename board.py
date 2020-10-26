457e34ea917a874a3c433b68d3210a3c0caf5d36
nbock <bocknolan@gmail.com>
2020-10-26 01:50:12 -0400

    MVP board generator

--- board.py
+++ board.py
@@ -2,6 +2,11 @@
 Board for a 9x9 Minesweeper board
 X represents a mine
 An integer represents mines surrounding an x,y point
+
+An array of tuples is meant to represent the board: (adjacent_mines, hidden_status)
+
+adjacent_mines can be -1, 0, 1, 2, 3
+hidden_status can be True if the square is covered and False otherwise
 '''
 
 import random
@@ -10,6 +15,19 @@ rows = 9
 cols = 9
 board = [[(0, True) for i in range(rows)] for j in range(cols)]
 
+
+def check(curr_row, curr_col, to_row, to_col):
+    '''
+
+    :param curr_row:
+    :param curr_col:
+    :param to_row:
+    :param to_col:
+    :return:
+    '''
+    return to_row >= 0 and to_col >= 0 and to_row < rows and to_col < cols and board[to_row][to_col][0] == -1
+
+
 mine_points = set()
 while len(mine_points) != 10:
     x = random.randint(0, rows - 1)
@@ -17,20 +35,50 @@ while len(mine_points) != 10:
     mine_points.add((x, y))
     board[x][y] = (-1, True)
 
+
+
 for row in range(rows):
     for col in range(cols):
+        if board[row][col][0] == -1:
+            continue
+
         mines = 0
-        if board[min(rows - 1, row + 1)][col][0] == -1:
-            mines += 1
-        if board[max(0, row - 1)][col][0] == -1:
-            mines += 1
-        if board[row][min(cols - 1, col + 1)][0] == -1:
-            mines += 1
-        if board[row][max(0, col - 1)][0] == -1:
+
+        # comparing bounded row and column
+
+        # left
+        if check(row, col, row, col - 1):
             mines += 1
 
-        if board[row][col][0] != -1:
-            board[row][col] = (mines, board[row][col][1])
+        # right
+        if check(row, col, row, col + 1):
+            mines += 1
 
+        # up left
+        if check(row, col, row - 1, col - 1):
+            mines += 1
+        # up
+        if check(row, col, row - 1, col):
+            mines += 1
+        # up right
+        if check(row, col, row - 1, col + 1):
+            mines += 1
+
+        # down left
+        if check(row, col, row + 1, col - 1):
+            mines += 1
+
+        # down
+        if check(row, col, row + 1, col):
+            mines += 1
+
+        # down right
+        if check(row, col, row + 1, col + 1):
+            mines += 1
+
+        board[row][col] = (mines, board[row][col][1])
+
+
+# just for seeing the board
 for i in range(rows):
     print(board[i])
